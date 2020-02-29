f3c7eb737a5f74b4216bdb05f0b5efc1dc1bfde4
braydentang1 <brayden.tang1@gmail.com>
2020-02-28 17:30:09 -0800

    fix documentation for frequency_encoder and con_encoder

--- R/frequency_encoder.R
+++ R/frequency_encoder.R
@@ -1,12 +1,9 @@
 #' frequency_encoder
 #'
 #' This function encodes categorical variables using the frequencies of each category.
-
 #'
 #' @param X_train A tibble representing the training data set containing some categorical features/columns.
 #' @param X_test A tibble representing the test set, containing some set of categorical features/columns.
-#' @param y A numeric vector or character vector representing the target variable. If the objective is "binary", then the vector
-#'  should only contain two unique values.
 #' @param cat_columns A character vector containing the names of the categorical columns in the tibble
 #' that should be encoded.
 #'
@@ -17,9 +14,8 @@
 #' @examples frequency_encoder(
 #' my_train,
 #' my_test,
-#' my_train$y,
 #' cat_columns = c("foo"))
-frequency_encoder <- function(X_train, X_test, y, cat_columns) {
+frequency_encoder <- function(X_train, X_test, cat_columns) {
 
   out <- list(train_processed, test_processed)
 
