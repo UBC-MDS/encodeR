fef25e429f2ad08d13c7af08653f2a6e16445ab9
Robert Pimentel <robilizando@yahoo.com>
2020-02-29 13:50:58 -0800

    Created initial onehot_encoder Docstring and help file

---
+++ R/onehot_encoder.R
@@ -0,0 +1,22 @@
+#' onehot_encoder
+#'
+#' This function encodes categorical variables using the popular onehot method for each category.
+#'
+#' @param X_train A tibble representing the training data set containing some categorical features/columns.
+#' @param X_test A tibble representing the test set, containing some set of categorical features/columns.
+#' @param cat_columns A character vector containing the names of the categorical columns in the tibble or
+#' data frame that should be encoded.
+#'
+#' @return A list containing with processed training and test sets, in which the named categorical
+#' columns are replaced with their encodings.
+#' @export
+#'
+#' @examples onehot_encoder(
+#' my_train,
+#' my_test,
+#' cat_columns = c("foo"))
+onehot_encoder <- function(X_train, X_test, cat_columns) {
+
+  out <- list(train_processed, test_processed)
+
+}
