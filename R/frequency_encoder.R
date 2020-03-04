b55717ccd26ba569f611db66c43bd6af5492df2a
bbaillie <baillie.bronwyn@gmail.com>
2020-03-04 11:50:19 -0800

    fixed to handle case where test contains class train did not, so does not produce NAs

--- R/frequency_encoder.R
+++ R/frequency_encoder.R
@@ -8,7 +8,8 @@ get_encoding <-  function(data, col){
 encode <- function(data,encoding_col,col){
   encoded_col <- data %>%
     dplyr::left_join(encoding_col) %>%
-    dplyr::mutate(!!sym(col) := freq) %>%
+    dplyr::mutate(!!sym(col) := if_else(is.na(freq), 0L, freq) # if encoding didn't exist encode value as 0
+                  ) %>% 
     dplyr::select(-freq)
   data[[sym(col)]] <- encoded_col[[sym(col)]]
   out <- data
