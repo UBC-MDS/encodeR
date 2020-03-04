09e8af17796d3f2300848a534ac3e5dc11fe7af4
bbaillie <baillie.bronwyn@gmail.com>
2020-03-03 19:31:20 -0800

    updated frequency_encoder function and added freq_encode function
    updated frequency_encoder function in frequency_encoder.R to mutate only provided columns and added freq_encode function to be applied to the chosen columns

--- R/frequency_encoder.R
+++ R/frequency_encoder.R
@@ -1,13 +1,18 @@
+
+encode_freq <- function(X) {
+  X
+}
+
 #' frequency_encoder
 #'
 #' This function encodes categorical variables using the frequencies of each category.
 #'
-#' @param X_train A tibble representing the training data set containing some categorical features/columns.
-#' @param X_test A tibble representing the test set, containing some set of categorical features/columns.
+#' @param X_train A `tibble` or `data.frame` representing the training data set containing some categorical features/columns.
+#' @param X_test A `tibble` or `data.frame` representing the test set, containing some set of categorical features/columns.
 #' @param cat_columns A character vector containing the names of the categorical columns in the tibble
 #' that should be encoded.
 #'
-#' @return A list with processed training and test sets, in which the named categorical
+#' @return A `list` with processed training and test sets, in which the named categorical
 #' columns are replaced with their encodings.
 #' @export
 #'
@@ -16,7 +21,8 @@
 #' my_test,
 #' cat_columns = c("foo"))
 frequency_encoder <- function(X_train, X_test, cat_columns) {
-
-  out <- list(train_processed, test_processed)
-
+  train_processed <- dplyr::mutate_if(X_train,~is.element(deparse(substitute(.x)), cat_columns), encode_freq)
+  test_processed <- X_test
+  out <- list( "train" = train_processed, "test" = test_processed)
 }
+
