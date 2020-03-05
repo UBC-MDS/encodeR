fcd6ceb12e7d1831db804d0c6cf27cae60a30337
bbaillie <baillie.bronwyn@gmail.com>
2020-03-04 20:02:29 -0800

    add testthat

---
+++ tests/testthat/test-frequency_encoder.R
@@ -0,0 +1,10 @@
+test_that("frequency_encoder() replaces categories with no encodings with 0", {
+  testing_data <- read_csv("../../data/testing_data.csv")
+  train <- testing_data %>%
+    dplyr::filter(train_test_3 == 'train')
+  test_missing_groups <- testing_data %>%
+    dplyr::filter(train_test_3 == 'test')
+  result <- frequency_encoder(train,test_missing_groups,c("feature_cat_chr"))
+  test_missing_groups_processed <- result$test_processed
+  expect_identical(anyNA(test_missing_groups_processed),FALSE)
+})
